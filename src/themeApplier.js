94783866875e813136ca7c188ec03502bf72e600
d āvis <dlektauers@gmail.com>
2021-09-05 20:02:57 +0300

    :recycle: Minor changes to code

--- src/themeApplier.js
+++ src/themeApplier.js
@@ -1,8 +1,4 @@
-// The WebExtensions Storage API has actual autism, PTSD, down syndrom, depression,
-// speech impediment, AIDS, HIV, ADHD, schizophrenia, trisomy 13, cancer,
-// and probably at least 18 other different diseases but doctors haven't 100% confirmed that yet
-
-// ...anyways this sets the theme to light by default if it hasn't been set
+// This sets the theme to light by default if it hasn't been set
 chrome.storage.sync.get(`theme`, (res) => {
 	if (Object.keys(res).length === 0) {
 		chrome.storage.sync.set({ theme: `light` });
