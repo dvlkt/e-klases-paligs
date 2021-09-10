018b449543adfb9978334fb6e2e9e186ef99e3a2
d āvis <dlektauers@gmail.com>
2021-09-10 20:24:36 +0300

    :bug: Fix a long-standing bug where favicons and logos don't change on Chrome

--- src/branding/main.js
+++ src/branding/main.js
@@ -1,37 +1,39 @@
-// Change favicon (currently broken on Chrome)
+// Change favicon
 try {
 	chrome.storage.sync.get(`theme`, (res) => {
 		let isBW = document.querySelector(`.ekl__header`) !== null; // As far as I can tell, this element only exists in the homepage
 		// This is to make the icon black and white when logged out
 
-		let iconUrl = `/res/${res.theme === `dark` ? `dark` : `light`}/icon-256${isBW ? `-bw` : ``}.png`;
-		// This ensures that if res.theme isn't either `dark` or `light`, it'll default to `light`
-
-		// Reads the icon file, code from here https://stackoverflow.com/a/50248437
-		// and here https://stackoverflow.com/a/18650249
-		fetch(chrome.runtime.getURL(iconUrl))
-			.then(response => response.blob())
-			.then(blob => {
-				var reader = new FileReader();
-				reader.readAsDataURL(blob);
-				reader.onloadend = () => {
-					var base64data = reader.result;
-
-					// Applies the actual icon
-					if (document.querySelector(`link[rel~="icon"]`) === null) {
-						let element = document.createElement(`link`);
-						element.rel = `icon`;
-						element.href = base64data;
-
-						document.head.appendChild(element);
-					} else {
-						document.querySelector(`link[rel~="icon"]`).href = base64data;
-					}
-				}
+		// Find out what the favicon should be
+		let faviconData;
+		switch (res.theme) {
+			case `dark`:
+				faviconData = isBW ? bwDarkFaviconData : darkFaviconData;
+				break;
+			default:
+				faviconData = isBW ? bwLightFaviconData : lightFaviconData;
+				break;
+		}
+		// Removes any existing icons
+		let linkElements = document.querySelectorAll(`link`);
+		for (let element of linkElements) {
+			if (element.rel.toLowerCase() === `icon` || element.rel.toLowerCase() === `shortcut icon`) {
+				element.setAttribute(`rel`, `none`);
 			}
-			);
+		}
+		// Applies the actual icon
+		let element = document.createElement(`link`);
+		element.setAttribute(`rel`, `shortcut icon`);
+		element.setAttribute(`type`, `image/x-icon`);
+		element.setAttribute(`href`, faviconData);
 
+		document.head.appendChild(element);
+		
 		// Change all of the original logos to the new ones
+		for (let logo of document.querySelectorAll(`.logo`)) {
+			logo.style.backgroundImage = `url("${res.theme === `dark` ? darkLogoData : lightLogoData}")`;
+		}
+		/*
 		// (this is basically the same code as above)
 		fetch(chrome.runtime.getURL(`/res/title-${res.theme === `dark` ? `dark` : `light`}.png`))
 			.then(response => response.blob())
@@ -46,7 +48,7 @@ try {
 					}
 				}
 			}
-		);
+		); */
 	});
 } catch (e) {
 	console.log(e);
