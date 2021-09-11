9ac9d9957a4958f80070593eaaf51c5190098b6a
d āvis <dlektauers@gmail.com>
2021-09-11 14:22:59 +0300

    :boom: Start implementing Family plan

--- src/animations.js
+++ src/animations.js
@@ -1,7 +1,7 @@
 // Top link animations
-for (let i of document.querySelectorAll(`.header-first-menu-item`)) {
-	if (i.querySelector(`.header-first-submenu`) !== null) {
-		let submenu = i.querySelector(`.header-first-submenu`);
+for (let i of document.querySelectorAll(`.header-first-menu-item, .header-second-menu-item`)) {
+	if (i.querySelector(`.header-first-submenu, .header-second-submenu`) !== null) {
+		let submenu = i.querySelector(`.header-first-submenu, .header-second-submenu`);
 
 		const onMouseEnter = () => {
 			if (submenu.style.display !== `block`) {
