4f53a3738bb0bb9f66e46609b02843dc6c11308b
d āvis <dlektauers@gmail.com>
2021-09-12 19:03:55 +0300

    :sparkles: Create a new changelog and add new paperclip icon

--- src/popup/themeApplier.js
+++ src/popup/themeApplier.js
@@ -1,9 +1,16 @@
 chrome.storage.sync.get(`theme`, (res) => {
-    if (res.theme === `dark`) {
-        document.body.className = `dark-theme`;
-        darkThemeBtn.className += ` theme-selected`;
-    } else {
-        document.body.className = `light-theme`;
-        lightThemeBtn.className += ` theme-selected`;
-    }
+	if (res.theme === `dark`) {
+		document.body.className = `dark-theme`;
+		if (darkThemeBtn !== undefined) darkThemeBtn.className += ` theme-selected`;
+
+		console.log(document.querySelector(`.news`))
+		if (document.querySelector(`.news`) !== null) {
+			for (element of document.querySelectorAll(`img`)) {
+				console.log(element.src)
+			}
+		}
+	} else {
+		document.body.className = `light-theme`;
+		if (lightThemeBtn !== undefined) lightThemeBtn.className += ` theme-selected`;
+	}
 });
