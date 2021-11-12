b989359e61116ebf4ec0f5875f086d61426da4c4
d āvis <dlektauers@gmail.com>
2021-11-12 16:52:09 +0200

    :sparkles: Improve the mail toolbar

--- src/tweaks/mail.js
+++ src/tweaks/mail.js
@@ -1,18 +1,25 @@
+const tryAddingListViewPatches = () => {
+	if (!location.href.includes(`SPA/Family#/mail/folder/`)) {
+		return;
+	}
+
+	if (document.querySelector(`.MailSearch__Input`) !== null) {
+		document.querySelector(`.MailSearch__Input`).setAttribute(`placeholder`, `Meklēt...`);
+	} else {
+		setTimeout(tryAddingListViewPatches, 20);
+	}
+}
 const tryAddingMailViewPatches = () => {
 	if (!location.href.includes(`SPA/Family#/mail/message/view/`)) {
-		if (document.querySelector(`.MailSearch__Input`) !== null) {
-			document.querySelector(`.MailSearch__Input`).setAttribute(`placeholder`, `Meklēt...`);
-		} else {
-			setTimeout(tryAddingMailViewPatches, 20);
-		}
-
 		return;
 	}
 
 	if (document.querySelector(`.MailMainAreaWrapper .ViewMessage .ViewMessage__MessageBody`) === null) {
 		setTimeout(tryAddingMailViewPatches, 20);
 	} else {
-		// Reorder the mail view elements
+		/*
+			Reorder the mail view elements
+		*/
 		let view = document.querySelector(`.ViewMessage`);
 		let personWrapper = document.querySelector(`.ViewMessage .ViewMessage__PersonWrapper`);
 		let metaInfo = document.querySelector(`.ViewMessage .ViewMessage__MetaInfo`);
@@ -21,7 +28,9 @@ const tryAddingMailViewPatches = () => {
 		view.appendChild(attachments);
 		metaInfo.appendChild(personWrapper);
 
-		// Add image previews
+		/*
+			Add file previews
+		*/
 		for (let attachmentElement of document.querySelectorAll(`.AttachmentList__ItemContainer`)) {
 			let fileType = attachmentElement.children[0].id.split(`.`).pop();
 
@@ -68,9 +77,60 @@ const tryAddingMailViewPatches = () => {
 		}
 	}
 }
+const tryAddingMailViewToolbarPatches = () => {
+	if (!location.href.includes(`SPA/Family#/mail/message/view/`)) {
+		return;
+	}
+
+	if (document.querySelector(`.MessageActions .TopToolbar__ButtonContainer`) === null) {
+		setTimeout(tryAddingMailViewToolbarPatches, 20);
+	} else {
+		/*
+			Add the star button to the toolbar
+		*/
+		let toolbar = document.querySelector(`.ViewMessage .MessageActions ul`);
+		let originalStarButton = document.querySelector(`.ViewMessage__Category.MessageFollowUpStatus`);
+		let starButton = document.createElement(`li`);
+		let starButtonState = `star-empty`;
+
+		if (originalStarButton.className.includes(`marked`)) {
+			starButtonState = `star-filled`;
+		} else if (originalStarButton.className.includes(`compleated`)) {
+			starButtonState = `check`;
+		}
+
+		starButton.className = `TopToolbar__ButtonContainer`;
+		starButton.innerHTML = `<button
+									title="Atzīmēt kā svarīgu"
+									aria-label="Atzīmēt kā svarīgu"
+									tabindex="0"
+									class="ButtonIcon TopToolbar__Button TopToolbar__Button--${starButtonState}">
+									&nbsp;
+								</button>`;
+		starButton.addEventListener(`click`, () => {
+			originalStarButton.click();
+
+			if (starButtonState === `star-empty`) {
+				starButtonState = `star-filled`;
+			} else if (starButtonState === `star-filled`) {
+				starButtonState = `check`;
+			} else {
+				starButtonState = `star-empty`;
+			}
+
+			starButton.children[0].className = `ButtonIcon TopToolbar__Button TopToolbar__Button--${starButtonState}`;
+		});
+		toolbar.insertBefore(starButton, toolbar.children[4]);
+	}
+}
+
 window.addEventListener(`urlChanged`, () => {
+	tryAddingListViewPatches();
 	tryAddingMailViewPatches();
+	tryAddingMailViewToolbarPatches();
 });
 window.addEventListener(`pageLoading`, () => {
+	tryAddingListViewPatches();
 	tryAddingMailViewPatches();
+	tryAddingMailViewToolbarPatches();
 });
