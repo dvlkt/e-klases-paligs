4692397c5c539b51d65b379af92820a6da897022
d āvis <dlektauers@gmail.com>
2021-11-30 20:42:52 +0200

    :bug: Fix custom pfp's showing through the mask of the profile icon on Chromium

--- src/tweaks/profilePicture.js
+++ src/tweaks/profilePicture.js
@@ -2,8 +2,7 @@ window.addEventListener(`pageLoading`, () => {
 	chrome.storage.local.get(`profilePicture`, (res) => {
 		if (Object.keys(res).length !== 0) {
 			if (document.querySelector(`.header-account-btn`) !== null) {
-				document.querySelector(`.header-account-btn`).style.mask = `none`;
-				document.querySelector(`.header-account-btn`).style.backgroundImage = `url("${res.profilePicture}")`;
+				document.querySelector(`.header-account-btn`).setAttribute(`style`, `mask-image: none; -webkit-mask-image: none; background: url("${res.profilePicture}"); background-size: cover;`);
 			}
 			if (document.querySelector(`.welcome-greeting .welcome-greeting-image.student-other`) !== null) {
 				document.querySelector(`.welcome-greeting .welcome-greeting-image.student-other`).style.filter = `none`;
