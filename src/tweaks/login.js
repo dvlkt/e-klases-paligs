a4688147bec9fe079b05ccb8500db2cf64d775c8
d āvis <dlektauers@gmail.com>
2021-12-04 11:34:04 +0200

    :sparkles: Bring the improved login system to all places

--- src/tweaks/login.js
+++ src/tweaks/login.js
@@ -3,13 +3,13 @@ window.addEventListener(`pageLoading`, () => {
 		 Add the "Remember me" checkbox
 	*/
 	// Check if the login box is on the page
-	if (document.querySelector(`.login-form, form.li-LoginForm`) !== null) {
-		let loginElementForm = document.querySelector(`aside.hidden-xs .login-form .form-content form, .li-LoginInputContainer_Layer form.li-LoginForm`);
-		let loginButton = loginElementForm.querySelector(`.li-LoginInputContainer_Layer .li-LoginForm_Row:nth-child(3), button`);
+	if (document.querySelector(`.login-form, form.li-LoginForm, .MainBody .InputForm`) !== null) {
+		let loginElementForm = document.querySelector(`aside.hidden-xs .login-form .form-content form, .li-LoginInputContainer_Layer form.li-LoginForm, .MainBody .InputForm form`);
+		let loginButton = loginElementForm.querySelector(`.li-LoginInputContainer_Layer .li-LoginForm_Row:nth-child(3), .InputForm_ButtonContainer, button`);
 
 		let showPasswordButtonElement = document.createElement(`div`);
 		showPasswordButtonElement.className = `show-password-btn`;
-		loginElementForm.querySelector(`.form-group.relative:nth-child(2), .li-LoginForm_Row:nth-child(3)`).appendChild(showPasswordButtonElement);
+		loginElementForm.querySelector(`.form-group.relative:nth-child(2), .li-LoginForm_Row:nth-child(3), .InputForm_Row:nth-child(2)`).appendChild(showPasswordButtonElement);
 
 		showPasswordButtonElement.addEventListener(`click`, () => {
 			let passwordInputElement = loginElementForm.querySelector(`input[placeholder="Parole"]`);
@@ -40,27 +40,18 @@ window.addEventListener(`pageLoading`, () => {
 	/*
 		Save and autofill the username & password if the "Remember me" checkbox has been checked
 	*/
-	if (document.querySelector(`.login-form, form.li-LoginForm`) !== null) {
+	if (document.querySelector(`.login-form, form.li-LoginForm, .MainBody .InputForm`) !== null) {
 
 		// Autofill
 		chrome.storage.sync.get([`loginDetails`], (res) => {
 			// For some reason when the page loads, the type of the password input is "private", not "password". Let's change that!
-			if (document.querySelectorAll(`.login-form .form-control[type="private"]`)[1] !== undefined) {
-				document.querySelectorAll(`.login-form .form-control[type="private"]`)[1].type = `password`;
-			}
-			if (document.querySelector(`form.li-LoginForm [type="private"]`) !== undefined) {
-				document.querySelector(`form.li-LoginForm [type="private"]`).type = `password`;
+			if (document.querySelector(`.row .login-form .form-control[type="private"], form.li-LoginForm [placeholder="Parole"], .InputForm_Row [type="private"]`) !== undefined) {
+				document.querySelector(`.row .login-form .form-control[type="private"], form.li-LoginForm [placeholder="Parole"], .InputForm_Row [type="private"]`).type = `password`;
 			}
 
-			// Apply the password
-			if (document.querySelector(`.login-form`) !== null) {
-				document.querySelectorAll(`.login-form .form-control[type="text"]`)[1].value = res.loginDetails?.username || ``;
-				document.querySelector(`.login-form .form-control[type="password"]`).value = res.loginDetails?.password || ``;
-
-			} else if (document.querySelector(`form.li-LoginForm`) !== null) {
-				document.querySelector(`form.li-LoginForm [placeholder="Lietotājvārds"]`).value = res.loginDetails?.username || ``;
-				document.querySelector(`form.li-LoginForm [placeholder="Parole"]`).value = res.loginDetails?.password || ``;
-			}
+			// Apply the information
+			document.querySelector(`.row .login-form .form-control[type="text"], form.li-LoginForm [placeholder="Lietotājvārds"], .InputForm_Row [placeholder="Lietotājs"]`).value = res.loginDetails?.username || ``;
+			document.querySelector(`.row .login-form .form-control[type="password"], form.li-LoginForm [placeholder="Parole"], .InputForm_Row [placeholder="Parole"]`).value = res.loginDetails?.password || ``;
 
 			// If there is any information saved, that means the checkbox should be checked as well
 			if (res.loginDetails !== null && (res.loginDetails?.username !== `` || res.loginDetails?.username !== ``)) {
@@ -69,12 +60,12 @@ window.addEventListener(`pageLoading`, () => {
 		});
 
 		// Saving
-		document.querySelectorAll(`.login-form .btn`)[1].addEventListener(`click`, () => {
+		document.querySelector(`.row .login-form .btn, form.li-LoginForm button.li-ButtonDefault-login, .InputForm_ButtonContainer .ButtonDefault`).addEventListener(`click`, () => {
 			if (document.querySelector(`#rememberPassword`).checked) {
 				chrome.storage.sync.set({
 					loginDetails: {
-						username: document.querySelectorAll(`.login-form .form-control[type="text"]`)[1].value,
-						password: document.querySelector(`.login-form .form-control[type="password"]`).value
+						username: document.querySelector(`.row .login-form .form-control[type="text"], form.li-LoginForm [placeholder="Lietotājvārds"], .InputForm_Row [placeholder="Lietotājs"]`).value,
+						password: document.querySelector(`.row .login-form .form-control[type="password"], form.li-LoginForm [placeholder="Parole"], .InputForm_Row [placeholder="Parole"]`).value
 					}
 				});
 			} else {
