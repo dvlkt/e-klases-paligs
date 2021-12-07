38880838b96bb99ab0db8dc0bfb9288014194a40
d āvis <dlektauers@gmail.com>
2021-12-07 19:22:56 +0200

    :sparkles: Make the logo change it's hue everywhere

--- src/popup/js/themeApplier.js
+++ src/popup/js/themeApplier.js
@@ -29,6 +29,18 @@ export const loadTheme = () => {
 			isRainbowActivated = true;
 		}
 
+		// Apply the theme color to the logo
+		for (let element of document.querySelectorAll(`img`)) {
+			if (element.src.includes(`title`)) {
+				let r = Number(`0x${res.themeColor[1]}${res.themeColor[2]}`);
+				let g = Number(`0x${res.themeColor[3]}${res.themeColor[4]}`);
+				let b = Number(`0x${res.themeColor[5]}${res.themeColor[6]}`);
+				let hue = rgbToHsv(r, g, b)[0] * 360;
+
+				element.style.filter = `hue-rotate(${hue - 215}deg)`;
+			}
+		}
+
 		// Apply the corner radius
 		document.querySelector(`:root`).style.setProperty(`--corner-radius`, `${res.cornerRadius}px`);
 	});
@@ -38,7 +50,13 @@ loadTheme();
 setInterval(() => {
 	if (isRainbowActivated) {
 		let rainbowHueValue = Math.floor(Date.now() / 50) % 360;
+
 		document.querySelector(`:root`).style.setProperty(`--theme-color`, `hsl(${rainbowHueValue}, 100%, 50%)`);
+		for (let element of document.querySelectorAll(`img`)) {
+			if (element.src.includes(`title`)) {
+				element.style.filter = `hue-rotate(${rainbowHueValue - 215}deg)`;
+			}
+		}
 	}
 }, 20);
 
@@ -51,3 +69,31 @@ chrome.runtime.onMessage.addListener((request) => {
 		loadTheme();
 	}
 });
+
+
+/*
+	RGB to HSV function, taken and modified from https://gist.github.com/mjackson/5311256
+*/
+const rgbToHsv = (r, g, b) => {
+	r /= 255, g /= 255, b /= 255;
+
+	var max = Math.max(r, g, b), min = Math.min(r, g, b);
+	var h, s, v = max;
+
+	var d = max - min;
+	s = max == 0 ? 0 : d / max;
+
+	if (max == min) {
+		h = 0; // achromatic
+	} else {
+		switch (max) {
+			case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+			case g: h = (b - r) / d + 2; break;
+			case b: h = (r - g) / d + 4; break;
+		}
+
+		h /= 6;
+	}
+
+	return [h, s, v];
+}
