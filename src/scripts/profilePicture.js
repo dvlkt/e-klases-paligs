857ffa5230ad1e27829a87e9d7f76355738aaa5f
d āvis <dlektauers@gmail.com>
2021-12-11 17:26:44 +0200

    :sparkles: Make the profile picture show up in settings

--- src/scripts/profilePicture.js
+++ src/scripts/profilePicture.js
@@ -8,6 +8,18 @@ window.addEventListener(`pageLoading`, () => {
 				document.querySelector(`.welcome-greeting .welcome-greeting-image.student-other`).style.filter = `none`;
 				document.querySelector(`.welcome-greeting .welcome-greeting-image.student-other`).style.backgroundImage = `url("${res.profilePicture}")`;
 			}
+			if (window.location.href.includes(`/Family/Settings/Profiles`)) {
+				let styleElement = document.createElement(`style`);
+				styleElement.innerHTML = `
+					html .pupils-wrap .item .pupil-block-header::after {
+						mask-image: none !important;
+						-webkit-mask-image: none !important;
+						background-image: url("${res.profilePicture}") !important;
+						background-size: contain !important;
+					}
+				`;
+				document.body.appendChild(styleElement);
+			}
 		}
 	});
 });
